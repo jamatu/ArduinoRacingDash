63a18b000d08006d15f55ded80c4356971984485
Turnermator13 <turnermator13@outlook.com>
2014-10-20 19:27:09 +0100

    Remove stats link stuff from version file

--- acSLI/acSLIApp/updater.py
+++ acSLI/acSLIApp/updater.py
@@ -42,8 +42,7 @@ class Updater:
                 versionStr = re.findall(r"\'(.+?)\'", str(versionFile.read()))[0]
                 self.remoteVersion = versionStr.split("|")[0]
                 self.reqArduinoUpdate = bool(versionStr.split("|")[1])
-                #self.statsURL = versionStr.split("|")[2]
-                self.changeLog = versionStr.split("|")[3]
+                self.changeLog = versionStr.split("|")[2]
                 self.statsURL = "/Q0hICb"
                 conn.close()
         except Exception as e:
